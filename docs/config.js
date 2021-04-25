519c90843da670c4dc66851b7c563721418ce568
Hidenori <hfu@users.noreply.github.com>
2021-04-25 10:10:26 +0900

    Create config.js

---
+++ docs/config.js
@@ -0,0 +1,33 @@
+var config = {
+  "accessToken": "pk.eyJ1IjoiaGZ1IiwiYSI6ImlRSGJVUTAifQ.rTx380smyvPc1gUfZv1cmw",
+  "byline": "by the United Nations Vector Tile Toolkit community",
+  "chapters": [
+    {
+      "alignment": "right",
+      "callback": "''",
+      "title": "ストーリーのタイトル",
+      "description": "ストーリーの記述",
+      "hidden": false,
+      "id": "s1",
+      "location": {
+        "bearing": 115.6,
+        "center": [
+          140.1270875,
+          36.0520654
+        ],
+        "pitch": 48,
+        "zoom": 18.77
+      },
+      "mapAnimation": "flyTo",
+      "onChapterEnter": [],
+      "onChapterExit": [],
+      "rotateAnimation": true
+    }
+  ],
+  "footer": "フッター",
+  "showMarkers": false,
+  "style": "https://optgeo.github.io/fgd-tkb/style.json",
+  "subtitle": "サブタイトル",
+  "theme": "light",
+  "title": "タイトル"
+}
